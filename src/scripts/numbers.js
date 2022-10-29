d2d8129716b83e92906d9f823dfe2317b741490d
Ольга Малышева <olga.maysheva.smm@gmail.com>
2022-10-29 16:47:00 +0300

    первое задание

--- src/scripts/numbers.js
+++ src/scripts/numbers.js
@@ -1,20 +1,31 @@
+const input = document.querySelector('.array');
+const error = document.querySelector('.errorMessage');
 const result = document.querySelector('.result');
 const sort = document.querySelector('.sort');
+const total = document.querySelector('.total');
+let array = [];
+input.addEventListener('change', ()=>{
+  error.innerHTML = '';
+ if(input.value != '' && !/[^0-9\.]/g.test(input.value)){
+  array.push(Number(input.value));
+  input.value = '';
+ } else {
+  error.innerHTML=`Введите цифры!`;
+ }
+});
 
-function sumInput() {
-  let array = [];
-  while (true) {
-    const number = prompt('Введите число:');
-    if (number == '')break;
-    array.push(+number);
-  }
+result.addEventListener('click', () => {
   array.sort();
-  let sum = 0;
-  for (let number of array) {
-    sum = sum + number;
-  }
-  console.log(sum);
-  result.innerHTML = `Итого получилось ${sum}`;
   sort.innerHTML = `Числа по возрасанию ${array.sort()}`;
+  let sum = 0; 
+  for (let input of array) {
+    sum = sum + input;
+  }
+  total.innerHTML = `Итого получилось ${sum}`;
+});
+
+function resetArray() {
+  newArray = [];
+  sort.innerHTML = "";
+  total.innerHTML = "";
 }
-sumInput();
